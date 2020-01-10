47559b346a8b0d7b595864830bf5c6524ee9fa97
JangJunHyeok <btrya23@gmail.com>
2020-01-10 23:59:15 +0900

    12 Table

---
+++ Table/Table/TableViewController.swift
@@ -0,0 +1,127 @@
+//
+//  TableViewController.swift
+//  Table
+//
+//  Created by 장준혁 on 2020/01/10.
+//  Copyright © 2020 장준혁. All rights reserved.
+//
+
+import UIKit
+
+var items = ["책 구매", "철수와 약속", "스터디 준비하기"]
+var itemsImageFile = ["cart.png", "clock.png", "pencil.png"]
+
+class TableViewController: UITableViewController {
+
+    @IBOutlet var tvListView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.navigationItem.leftBarButtonItem = self.editButtonItem // 오른쪽은 삭제가 있으니 수정을 왼쪽에 추가
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tvListView.reloadData()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+        // 테이블 안에 섹션이 한 개
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return items.count
+        // 섹션당 열의 개수는 items
+    }
+
+    // 테이블 이미지가 생성되었다 !!!
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+
+        cell.textLabel?.text = items[(indexPath as NSIndexPath).row]    // 내용, "책 구매" 등등 ...
+        cell.imageView?.image = UIImage(named: itemsImageFile[(indexPath as NSIndexPath).row])  // 이미지, "cart.png" 등등 ...
+        
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    // 목록 삭제하기
+    // Override to support editing the table view.
+    // 교재의 fix 부분은 수정 됨
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            
+            items.remove(at: (indexPath as NSIndexPath).row)
+            itemsImageFile.remove(at: (indexPath as NSIndexPath).row)
+            
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    
+    // !!! 함수 추가, Delete 를 삭제로
+    override func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
+        return "삭제"
+    }
+
+    
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+        let itemToMove = items[(fromIndexPath as NSIndexPath).row]
+        let itemImageToMove = itemsImageFile[(fromIndexPath as NSIndexPath).row]
+        items.remove(at: (fromIndexPath as NSIndexPath).row)
+        itemsImageFile.remove(at: (fromIndexPath as NSIndexPath).row)
+        items.insert(itemToMove, at: (fromIndexPath as NSIndexPath).row)
+        itemsImageFile.insert(itemImageToMove, at: (fromIndexPath as NSIndexPath).row)
+
+    }
+    
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+    
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        if segue.identifier == "sgDetail" {
+            let cell = sender as! UITableViewCell
+            let indexPath = self.tvListView.indexPath(for: cell)
+            let detailView = segue.destination as! DetailViewController
+            detailView.reciveItem(items[((indexPath as NSIndexPath?)?.row)!])
+        }
+    }
+    
+
+}
