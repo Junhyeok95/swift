472fd339658ab52d3f11c6eb5ada431c51b193d5
JangJunHyeok <btrya23@gmail.com>
2020-01-13 21:09:17 +0900

    AutoLayout

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by 장준혁 on 2020/01/13.
+//  Copyright © 2020 장준혁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
