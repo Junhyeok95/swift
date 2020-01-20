ecd0e3e52e16ae0211b0618b0a0f5f208e0b9171
JangJunHyeok <btrya23@gmail.com>
2020-01-20 23:10:13 +0900

    14 MoviePlayer ...

---
+++ MoviePlayer/MoviePlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviePlayer
+//
+//  Created by 장준혁 on 2020/01/20.
+//  Copyright © 2020 장준혁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
