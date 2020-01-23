e571d3c29e7a82eb376304364b1f091a79ac140b
JangJunHyeok <btrya23@gmail.com>
2020-01-23 23:11:59 +0900

    14 MoviePlayer

--- MoviePlayer/MoviePlayer/ViewController.swift
+++ MoviePlayer/MoviePlayer/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVKit // 비디오 킷
 
 class ViewController: UIViewController {
 
@@ -16,5 +17,29 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func btnPlayInternalMovie(_ sender: UIButton) {
+        // 내부 파일
+        let filePath:String? = Bundle.main.path(forResource: "FastTyping", ofType: "mp4") // 경로
+        let url = NSURL(fileURLWithPath: filePath!) // NSURL 형식으로 변환
+        
+        playVideo(url: url)
+    }
+    @IBAction func btnPlayExternalMovie(_ sender: Any) {
+        // 외부 파일
+        let url = NSURL(string: "https://dl.dropboxusercontent.com/s/e38auz050w2mvud/Fireworks.mp4")!
+        
+        playVideo(url: url)
+    }
+    
+    // 플레이 !!
+    private func playVideo(url: NSURL) {
+        // 실행
+        let playerController = AVPlayerViewController() // 인스턴스
+        let player = AVPlayer(url: url as URL) // URL로 인스턴스
+        playerController.player = player
+        self.present(playerController, animated: true) {
+            player.play() // 플레이 !
+        }
+    }
 }
 
